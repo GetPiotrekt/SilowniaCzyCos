51bd1bbacb61765e41aeb611f30901a1bd0cbfed
ZioXel <lukas17365@gmail.com>
2023-10-09 17:41:39 +0200

    ass

---
+++ src/mainCode/ExerciseTypes.java
@@ -0,0 +1,20 @@
+package mainCode;
+
+public class ExerciseTypes {
+
+    private void pullUps(){
+
+        float userWeight;
+        float objectWeight=0;
+        float additionalWeight=0;
+        float volume=0;
+        int reps=0;
+        int sets=0;
+        char weightType;
+        char Symbol;
+
+
+    }
+
+
+}
