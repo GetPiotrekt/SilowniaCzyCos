6103076223c72c157ee6c5711be463216fc99f7e
GetPiotrekt <92060850+GetPiotrekt@users.noreply.github.com>
2023-11-17 14:03:49 +0100

    Zaktualizowane, trochę pododawane
     Jest jak jest, będzie jak będzie

--- src/mainCode/Main.java
+++ src/mainCode/Main.java
@@ -1,25 +1,56 @@
-package mainCode;
-
-
-import java.security.spec.ECField;
 import java.util.Scanner;
+import java.util.Date;
 
 public class Main {
 
-    public static Scanner scanner;
-
-
     public static void main(String[] args) {
+        Zapisywanie.zapiszDate(); // zapisanie daty
+        int aktualneCwiczenie = 1;
+        Scanner scanner = new Scanner(System.in);
 
-        ExerciseTypes test = new ExerciseTypes();
+        Date startTime = new Date(); // Rozpoczęcie pomiaru czasu
 
-        test.pullUps();
+        while (aktualneCwiczenie != 4) {
+            System.out.println("\nWybierz cwiczenie z listy:");
+            System.out.println("1. Wyciskanie sztangi na lawce plaskiej");
+            System.out.println("2. Podciaganie na drazku");
+            System.out.println("3. Martwy ciag");
+            System.out.println("4. Pokaż wyniki");
+            System.out.println("5. Koniec treningu\n");
+            System.out.print("Twoj wybor: ");
 
+            int wybor = scanner.nextInt();
 
+            switch (wybor) {
+                case 1:
+                    Cwiczenia.lawkaPlaska();
+                    break;
+                case 2:
+                    Cwiczenia.podciaganie();
+                    break;
+                case 3:
+                    Cwiczenia.martwyCiag();
+                    break;
+                case 4:
+                    Zapisywanie.wyswietlZawartosc();
+                    break;
+                case 5:
+                    Date endTime = new Date(); // Zakończenie pomiaru czasu
+                    long totalTime = endTime.getTime() - startTime.getTime();
+                    System.out.println("Czas treningu: " + Zapisywanie.formatujCzas(totalTime));
 
+                    Zapisywanie.zapisCzasuTreningu(totalTime);
 
+                    System.out.println("Zakonczono program.");
+                    aktualneCwiczenie = 4;
+                    break;
+                default:
+                    System.out.println("Niepoprawny wybor. Wybierz ponownie.");
+                    break;
+            }
+        }
+
+        System.out.println("Program zakonczyl dzialanie. Wcisnij 'enter', aby zamknac");
+        scanner.close();
     }
-
-
-
 }
