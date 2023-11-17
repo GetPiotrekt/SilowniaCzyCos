7f1f57c24892b73b6541cbedd2175e9bc63da8eb
GetPiotrekt <92060850+GetPiotrekt@users.noreply.github.com>
2023-11-17 19:04:26 +0100

    ulepszony Zapisywanie.java

--- src/mainCode/Zapisywanie.java
+++ src/mainCode/Zapisywanie.java
@@ -1,3 +1,5 @@
+package mainCode;
+
 import java.io.*;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
@@ -16,6 +18,8 @@ public class Zapisywanie {
             e.printStackTrace();
         }
 
+        zapiszDateDoOdczytu(); // Dodaj datę na początku pliku "odczyt.txt"
+
         try {
             BufferedWriter postepy = new BufferedWriter(new FileWriter("odczyt.txt", true));
             postepy.write(cw + " ; " + nrSer + " ; " + ciez + " ; " + powt + " ; " + obj + "\n");
@@ -26,14 +30,20 @@ public class Zapisywanie {
     }
 
     public static void wyswietlZawartosc() {
-        try {
-            BufferedReader czytaj = new BufferedReader(new FileReader("odczyt.txt"));
-            String linia;
-            System.out.println("Cwiczenie; Numer Serii; Ciezar; ilosc powtorzen; objetosc;");
-            while ((linia = czytaj.readLine()) != null) {
-                System.out.println(linia);
+        BufferedReader czytaj = new BufferedReader(new FileReader("odczyt.txt"));
+        String linia;
+        System.out.println("Data; Cwiczenie; Numer Serii; Ciezar; ilosc powtorzen; objetosc;");
+
+        while ((linia = czytaj.readLine()) != null) {
+            String[] elements = linia.split(" ; ");
+
+            if (elements.length == 6) {
+                String formattedDate = elements[0];
+                formattedDate = formattedDate.substring(0, 2) + "." + formattedDate.substring(2, 4) + "." + formattedDate.substring(4);
+                System.out.println(formattedDate + " ; " + elements[1] + " ; " + elements[2] + " ; " + elements[3] + " ; " + elements[4] + " ; " + elements[5]);
             }
-            czytaj.close();
+        }
+        czytaj.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -47,6 +57,23 @@ public class Zapisywanie {
 
             FileWriter plik = new FileWriter("wynik.txt", true);
             plik.write("Rozpoczęcie treningu: " + formattedDate + "\n\n");
+            plik.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void zapiszDateDoOdczytu() {
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("ddMMyyyy");
+            Date currentDate = new Date();
+            String formattedDate = dateFormat.format(currentDate);
+
+            // Dodanie zer przed jednocyfrowymi dniami i miesiącami
+            formattedDate = formattedDate.substring(0, 2) + formattedDate.substring(2, 4) + formattedDate.substring(4);
+
+            FileWriter plik = new FileWriter("odczyt.txt", true);
+            plik.write(formattedDate + " ; ");
             plik.close();
         } catch (IOException e) {
             e.printStackTrace();
